daaf7a3e80d879befc5691ef8b2c4d13ad1bd95c
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-08-20 14:46:44 -0300

    Adicionados Times da Serie B

--- Exercicio 1.bat
+++ Exercicio 1.bat
@@ -4,3 +4,6 @@ md "Serie A" "Serie B"
 cd "Serie A"
 md Atletico-MG Palmeiras Fortaleza Bragantino Flamengo Athletico-PR Atletico-GO Ceara Internacional Santos
 Corinthians Juventude Bahia "São Paulo" Fluminense Cuiaba Sport America-MG Gremio Chapecoense
+cd..
+cd "Serie B"
+md Coritiba Goias Avai CRB "Sampaio Correa" Guarani Nautico Botafogo Operario-PR CSA Vasco Remo Brusque Cruzeiro "Vila Nova" "Ponte Preta" Londrina Vitoria Confianca "Brasil de Pelotas"
