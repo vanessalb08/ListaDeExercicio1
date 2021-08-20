a166cfdd458298ef6aa1d705f9b2d6dec0f4406a
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-08-20 14:34:27 -0300

    Adicionados times da serie A

--- Exercicio 1.bat
+++ Exercicio 1.bat
@@ -1,3 +1,6 @@
 md "Todos os clubes"
 cd "Todos os clubes"
 md "Serie A" "Serie B"
+cd "Serie A"
+md Atletico-MG Palmeiras Fortaleza Bragantino Flamengo Athletico-PR Atletico-GO Ceara Internacional Santos
+Corinthians Juventude Bahia "São Paulo" Fluminense Cuiaba Sport America-MG Gremio Chapecoense
